68d87c8fa3c84ab33d173260329069f9f908bb3b
Richard Groves <rgroves8973@gmail.com>
2025-05-12 08:33:31 -0600

    [Test] Refactor baud_gen_tb to accommodate core refactor (GREEN)

--- test/baud_gen_tb.sv
+++ test/baud_gen_tb.sv
@@ -23,6 +23,16 @@ module baud_gen_tb;
 		test_case_t'{dl: 16'd54,  prc: 4'd0}	// 15200 bps
 	};
 
+
+
+    // Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Tick counters
+    logic [31:0] baud_clk_count = 0;
+    logic [31:0] baud_clk_target = 0;
+    logic [31:0] sample_clk_count = 0;
+    logic [31:0] sample_clk_target = 0;
+	
+	// DUT
     logic clk;
     logic reset;
     DL_word_t divisor_latch;
@@ -31,32 +41,32 @@ module baud_gen_tb;
     logic baud_tick;
     logic sample_tick;
     logic active;
+	logic enable_sample;
+	logic enable_baud;
 
-    // Instantiate DUT
-    baud_gen #(
-        .DL_WIDTH(DL_WIDTH),
-        .PSD_WIDTH(PSD_WIDTH)
-    ) dut (
-        .clk(clk),
-        .reset(reset),
-        .divisor_latch(divisor_latch),
-        .psd(psd),
-        .new_baud(new_baud),
-        .baud_tick(baud_tick),
-        .sample_tick(sample_tick),
-        .active(active)
-    );
+    // Instantiate DUT ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	baud_gen #(
+		.DL_WIDTH(DL_WIDTH),
+		.PSD_WIDTH(PSD_WIDTH)
+	) dut (
+		.clk(clk),
+		.reset(reset),
+		.divisor_latch(divisor_latch),
+		.psd(psd),
+		.new_baud(new_baud),
+		.enable_sample(enable_sample),
+		.enable_baud(enable_baud),
+		.baud_tick(baud_tick),
+		.sample_tick(sample_tick),
+		.active(active)
+	);
 
 
-    // Clock generation
+
+    // Clock generation ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     always #(CLK_PERIOD/2) clk = ~clk;
 
-    // Tick counters
-    logic [31:0] baud_clk_count = 0;
-    logic [31:0] baud_clk_target = 0;
-    logic [31:0] sample_clk_count = 0;
-    logic [31:0] sample_clk_target = 0;
-
+	// Tick counters ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     always_ff @(posedge clk) begin
         if (active) begin
 			baud_clk_count <= baud_clk_count + 1;
@@ -67,10 +77,8 @@ module baud_gen_tb;
         end
     end
 
-    // Main test logic
+    // Main test logic ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     initial begin
-        $display("Starting baud_gen testbench...");
-
         clk = 0;
         reset = 1;
         new_baud = 0;
@@ -80,8 +88,10 @@ module baud_gen_tb;
 
         reset = 0;
         @(posedge clk);
+		enable_sample = 1;
+		enable_baud = 1;
 		
-        // Test 0
+        // Test default DL and PSD
 		baud_clk_target = 1_048_576;
 		sample_clk_target = 65_536;
 		
@@ -89,24 +99,24 @@ module baud_gen_tb;
 			$display("Failed. Active signal is not valid!");
 			$finish;
 		end
-        wait (active); // wait for baud setup
+        wait (active);
 		
-        wait (sample_tick); // wait for sample tick
+        wait (sample_tick);
         if (sample_clk_count !== sample_clk_target) begin
 			$display("Failed sample tick test!");
 			$display("Expected clock count: %0d. Actual clock count: %0d.",sample_clk_target, sample_clk_count);
 			$stop;
         end
 
-        wait (baud_tick); // wait for sample tick
+        wait (baud_tick);
         if (baud_clk_count !== baud_clk_target) begin
 			$display("Failed baud tick test!");
 			$display("Expected clock count: %0d. Actual clock count: %0d.",sample_clk_target, baud_clk_count);
 			$stop;
         end
 
+        // Increment through test vector
         foreach (tests[i]) begin
-            // Apply test vector
             divisor_latch = tests[i].dl;
             psd = tests[i].prc;
 			new_baud = 1;
@@ -117,10 +127,10 @@ module baud_gen_tb;
 			sample_clk_target = divisor_latch * (psd + 1);
 			
 			
-			wait (!active); // wait for baud setup
-			wait (active); // wait for baud setup
+			wait (!active);
+			wait (active);
 			
-			wait (sample_tick); // wait for sample tick
+			wait (sample_tick);
 			if (sample_clk_count !== sample_clk_target) begin
 				$display("Failed sample tick test!");
 				$display("Expected clock count: %0d. Actual clock count: %0d.",sample_clk_target, sample_clk_count);
